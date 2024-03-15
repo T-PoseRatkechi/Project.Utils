5a9df7e6048eac0fe5cfe431e272113b2dcab712
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-03-15 00:36:53 -0500

    Add support for caching unicode strings.

--- StringsCache.cs
+++ StringsCache.cs
@@ -5,6 +5,7 @@ namespace Project.Utils;
 public static class StringsCache
 {
     private static readonly Dictionary<string, nint> cachedStrPtrs = new();
+    private static readonly Dictionary<string, nint> cachedStrPtrsUni = new();
 
     public static nint GetStringPtr(string str)
     {
@@ -15,6 +16,18 @@ public static class StringsCache
 
         var newPtr = Marshal.StringToHGlobalAnsi(str);
         cachedStrPtrs[str] = newPtr;
+        return newPtr;
+    }
+
+    public static nint GetStringPtrUni(string str)
+    {
+        if (cachedStrPtrsUni.TryGetValue(str, out var ptr))
+        {
+            return ptr;
+        }
+
+        var newPtr = Marshal.StringToHGlobalUni(str);
+        cachedStrPtrsUni[str] = newPtr;
         return newPtr;
     }
 }
